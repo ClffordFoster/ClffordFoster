442627e89533c0e98e24b44836bcb24fe8d20cd5
Clifford Foster <clifford.foster@smail.astate.edu>
2021-04-09 10:34:23 -0500

    createdschema

---
+++ Database/Hero.sql
@@ -0,0 +1,107 @@
+CREATE TABLE IF NOT EXISTS Users (
+    userID TEXT PRIMARY KEY,
+    username TEXT UNIQUE NOT NULL,
+    passwordHash TEXT NOT NULL,
+    role INTEGER NOT NULL DEFAULT 0, -- default to "user" role
+    email TEXT UNIQUE NOT NULL,
+    didVerifyEmail BOOLEAN NOT NULL DEFAULT 0 -- defaults to unverified
+);
+
+CREATE TABLE IF NOT EXISTS Characters (
+    -- Identifer's 
+    name TEXT NOT NULL,
+    characterID TEXT PRIMARY KEY,
+    FOREIGN KEY (userID) REFERENCES Users(userID),
+
+    
+
+    --Temp Stats 
+    HP Integer,
+    SP Integer,
+    MP Integer,
+    levels INTEGER DEFAULT 1, 
+    BLOODIED INTEGER, 
+
+    --ATTRIBUTES
+    STR (STR >=1 and STR <= 30) NOT NULL ,
+    CON (CON >=1 and CON <= 30) NOT NULL ,
+    DEX (DEX >=1 and DEX <= 30) NOT NULL,
+    INT (INT  >=1 and INT <= 30) NOT NULL,
+    WIS (WIS >=1 and WIS <= 30) NOT NULL,
+    CHA (CHA >=1 and CHA <= 30) NOT NULL,
+   
+   --MODIFERS 
+    STR_MOD Integer DEFAULT 0,
+    CON_MOD Integer DEFAULT 0,
+    DEX_MOD Integer DEFAULT 0,
+    INT_MOD Integer DEFAULT 0,
+    WIS_MOD Integer DEFAULT 0,
+    CHA_MOD Integer DEFAULT 0,
+
+    --SOFTSKILLS
+    Speed Integer,
+    Perceptiion Integer,
+    POISE Integer DEFAULT 3,
+
+    -- DEF STATS
+    ARM Integer DEFAULT 11,
+    EVA INTEGER DEFAULT 10, 
+    TGH INTEGER DEFAULT 10, 
+    DR INTEGER DEFAULT -1 
+    
+    --generalInformation
+    characterbackground TEXT,  
+    age TEXT NOT NULL,
+    height TEXT NOT NULL,
+    weight TEXT NOT NULL,
+    race TEXT NOT NULL,
+    subClass TEXT, 
+    trade TEXT, 
+    Title TEXT,
+    SkillPoints TEXT,
+    EXP Integer,
+    TNL Integer,
+    Personality TEXT, 
+    Orgin TEXT,
+    Languages TEXT, 
+    
+
+
+
+    -- skills, subclass, perks, and inventory 
+    FOREIGN KEY (skillID) REFERENCES Skills(skillID),
+    FOREIGN KEY (perkID) REFERENCES Perks(perkID),
+    FOREIGN KEY (inventoryID) REFERENCES Inventory(inventoryID),
+    
+);
+
+CREATE TABLE IF NOT EXISTS Skills(
+    skillID TEXT PRIMARY KEY,
+    skillName TEXT,
+    skillDescription TEXT, 
+    skillLevel Integer, 
+    category TEXT, 
+
+
+
+);
+
+CREATE TABLE IF NOT EXISTS Perks(
+    perkID TEXT PRIMARY KEY,
+    perkName TEXT, 
+    perkDiscription TEXT,
+
+
+);
+
+CREATE TABLE IF NOT EXISTS Inventory(
+    inventoryID TEXT PRIMARY KEY,
+    itemName TEXT, 
+        
+
+
+);
+
+TABLE WEAPON
+
+TABLE ARMOR
